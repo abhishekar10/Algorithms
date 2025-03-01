8cefdd74afd9388affe11d3010b8e4d4018b119b
Abhishek A R <getarabhishek@gmail.com>
2025-03-01 20:54:59 +0530

    Added Two Pointer Solution for max water

--- two-pointer.py
+++ two-pointer.py
@@ -6,4 +6,18 @@ while r < len(sampleList):
         sampleList[l],sampleList[r] = sampleList[r],sampleList[l]
         l+=1
     r+=1
-print(sampleList)
+# print(sampleList)
+
+#Finding two lines storing most water
+c = [1,8,6,2,5,4,8,3,7] #c short for container
+n,x = 0,0
+l,r = 0,len(c)-1 #n for minimum and x for maximum
+a = 0 #a for area
+while l <= r:
+    ta = min(c[l],c[r])*(r-l)
+    a = max(a,ta)
+    if(c[l] < c[r]):
+        l+=1
+    else:
+        r-=1
+print(a)
