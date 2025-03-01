b90c7803ee7ad312ea4d14865c77e1437c2fe72c
Abhishek A R <getarabhishek@gmail.com>
2025-03-01 19:38:32 +0530

    Added Two Pointer solution for moving zeroes

--- two-pointer.py
+++ two-pointer.py
@@ -1,1 +1,9 @@
-# Initial commit test
+#Move Zeroes
+sampleList = [0,1,0,3,12]
+l,r = 0,1
+while r < len(sampleList):
+    if sampleList[r] != 0 :
+        sampleList[l],sampleList[r] = sampleList[r],sampleList[l]
+        l+=1
+    r+=1
+print(sampleList)
