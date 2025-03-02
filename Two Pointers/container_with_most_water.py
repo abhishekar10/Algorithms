4e41769ad8f9147a024615777132f1477d772c50
Abhishek A R <getarabhishek@gmail.com>
2025-03-02 21:13:18 +0530

    Added Two Pointers Directory

--- Two Pointers/container_with_most_water.py
+++ Two Pointers/container_with_most_water.py
@@ -1,14 +1,3 @@
-#Move Zeroes
-sampleList = [0,1,0,3,12]
-l,r = 0,1
-while r < len(sampleList):
-    if sampleList[r] != 0 :
-        sampleList[l],sampleList[r] = sampleList[r],sampleList[l]
-        l+=1
-    r+=1
-# print(sampleList)
-
-#Finding two lines storing most water
 c = [1,8,6,2,5,4,8,3,7] #c short for container
 n,x = 0,0
 l,r = 0,len(c)-1 #n for minimum and x for maximum
